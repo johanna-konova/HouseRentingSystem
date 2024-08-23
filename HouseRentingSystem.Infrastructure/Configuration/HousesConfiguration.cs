d1521e6fd9d76d0e67655e7a34120986af07f0b5
johanna-konova <j.konova@abv.bg>
2024-08-23 21:33:13 +0300

    House configuration added

---
+++ HouseRentingSystem.Infrastructure/Configuration/HousesConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using HouseRentingSystem.Infrastructure.Models;
+
+namespace HouseRentingSystem.Infrastructure.Configurations
+{
+    internal class HousesConfiguration : IEntityTypeConfiguration<House>
+    {
+        public void Configure(EntityTypeBuilder<House> builder)
+        {
+            builder
+                .HasOne(h => h.Category)
+                .WithMany(c => c.Houses)
+                .HasForeignKey(h => h.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(h => h.Agent)
+                .WithMany()
+                .HasForeignKey(h => h.AgentId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
