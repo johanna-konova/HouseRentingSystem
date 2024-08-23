f4ad87fd7048068db047ebb603e879dcfc5d2237
johanna-konova <j.konova@abv.bg>
2024-08-23 19:08:45 +0300

    HouseRentingDbContext moved from HouseRentingSystem.Web to HouseRentingSystem.Infrastructure

---
+++ HouseRentingSystem.Infrastructure/HouseRentingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure
+{
+    public class HouseRentingDbContext : IdentityDbContext
+    {
+        public HouseRentingDbContext(DbContextOptions<HouseRentingDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
