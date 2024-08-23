cc3c00d3fdd8cef5d8acfc1ec0e33e72a03b747e
johanna-konova <j.konova@abv.bg>
2024-08-23 21:35:24 +0300

    Data models added and house configuration apply to HouseRentingDbContext

--- HouseRentingSystem.Infrastructure/HouseRentingDbContext.cs
+++ HouseRentingSystem.Infrastructure/HouseRentingDbContext.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HouseRentingSystem.Infrastructure.Configurations;
+using HouseRentingSystem.Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace HouseRentingSystem.Infrastructure
 {
-    public class HouseRentingDbContext : IdentityDbContext
+    public class HouseRentingDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public HouseRentingDbContext(DbContextOptions<HouseRentingDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<House> Houses { get; set; } = null!;
+        public DbSet<Agent> Agents { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new HousesConfiguration());
+
+            base.OnModelCreating(builder);
         }
     }
 }
