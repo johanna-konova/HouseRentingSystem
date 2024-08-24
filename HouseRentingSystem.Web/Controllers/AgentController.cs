5499c46b1cd0f13cee6c7181e86b27700f44ee5f
johanna-konova <j.konova@abv.bg>
2024-08-24 13:31:09 +0300

    Agent and house controllers with them actions added

---
+++ HouseRentingSystem.Web/Controllers/AgentController.cs
@@ -0,0 +1,23 @@
+﻿using HouseRentingSystem.Core.Models.Agents;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Controllers
+{
+	[Authorize]
+	public class AgentController : Controller
+	{
+		public IActionResult Index() => RedirectToAction(nameof(Become));
+
+		public async Task<IActionResult> Become()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Become(BecomeAgentFormModel model)
+		{
+			return RedirectToAction(nameof(HouseController.All), "Houses");
+		}
+	}
+}
