c0e8e6ec32e95586f291e1e1ca137c1d69344109
johanna-konova <j.konova@abv.bg>
2024-08-25 11:19:21 +0300

    Home controller modified

--- HouseRentingSystem.Web/Controllers/HomeController.cs
+++ HouseRentingSystem.Web/Controllers/HomeController.cs
@@ -1,27 +1,28 @@
 using HouseRentingSystem.Core.Models;
+using HouseRentingSystem.Core.Models.Home;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace HouseRentingSystem.Web.Controllers
+namespace HouseRentingSystem.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View(new IndexViewModel());
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
