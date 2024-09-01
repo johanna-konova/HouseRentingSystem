778f209799521c30d28a73a568c754db104b7e8c
johanna-konova <j.konova@abv.bg>
2024-09-01 12:52:37 +0300

    Unit of work pattern applied through repository

---
+++ HouseRentingSystem.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+        IQueryable<T> AllAsNoTracking<T>() where T : class;
+    }
+}
---
+++ HouseRentingSystem.Infrastructure/Common/Repository.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(HouseRentingDbContext _context)
+        {
+            context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+            => context.Set<T>();
+
+        public IQueryable<T> All<T>() where T : class
+            => DbSet<T>();
+
+        public IQueryable<T> AllAsNoTracking<T>() where T : class
+            => DbSet<T>().AsNoTracking();
+    }
+}
