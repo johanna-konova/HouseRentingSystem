f49a5462cf05c0eb1862338b541e5af9176cc150
johanna-konova <j.konova@abv.bg>
2024-09-02 13:26:21 +0300

    Messege types and constants created

--- HouseRentingSystem.Core/Constants/MessageConstants.cs
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -2,8 +2,10 @@
 {
     public static class MessageConstants
     {
-        public const string PhoneExists = "Phone number already exists. Enter another one";
+        public const string AlreadyAgent = "You are already an agent.";
 
-        public const string HasRents = "You should have no rents to become an agent";
+        public const string PhoneExists = "Phone number already exists. Enter another one.";
+
+        public const string HasRents = "You should have no rents to become an agent.";
     }
 }
