45e9826e6406aea00a7de17ac31ede8877cc53d2
johanna-konova <j.konova@abv.bg>
2024-09-02 23:10:52 +0300

    More message constants added

--- HouseRentingSystem.Core/Constants/MessageConstants.cs
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -1,11 +1,23 @@
 ﻿namespace HouseRentingSystem.Core.Constants
 {
     public static class MessageConstants
-    {
-        public const string AlreadyAgent = "You are already an agent.";
+	{
+		public const string RequiredMessage = "The {0} field is required.";
+
+		public const string InvalidStringLength = "The {0} field must be between {2} and {1} characters long.";
+
+        public const string InvalidUrlFormat = "The {0} field is not valid URL format.";
+
+        public const string InvalidPricePerMonthRange = "The {0} field must be a positive number and less than {2} leva.";
+
+		public const string AlreadyAgent = "You are already an agent.";
+
+        public const string MustBeAgent = "You must be an agent to add new house.";
 
         public const string PhoneExists = "Phone number already exists. Enter another one.";
 
         public const string HasRents = "You should have no rents to become an agent.";
+
+        public const string NonExistentCategory = "Category does not exist.";
     }
 }
