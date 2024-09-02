9dcbf5bda659f1998698da95d41dc28654ce07fb
johanna-konova <j.konova@abv.bg>
2024-09-02 11:14:20 +0300

    Claims principal extended

---
+++ HouseRentingSystem.Web/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Security.Claims
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid Id(this ClaimsPrincipal user)
+            => Guid.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)!);
+    }
+}
