8259ccfec771f42be1271673cf5441ed734231d1
johanna-konova <j.konova@abv.bg>
2024-09-02 13:24:29 +0300

    BecomeAgentFormModel updated

--- HouseRentingSystem.Core/Models/Agents/BecomeAgentFormModel.cs
+++ HouseRentingSystem.Core/Models/Agents/BecomeAgentFormModel.cs
@@ -1,6 +1,15 @@
-﻿namespace HouseRentingSystem.Core.Models.Agents
+﻿using System.ComponentModel.DataAnnotations;
+
+using static HouseRentingSystem.Infrastructure.DataConstants.Agent;
+
+namespace HouseRentingSystem.Core.Models.Agents
 {
 	public class BecomeAgentFormModel
 	{
+		[Required]
+		[StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
+		[Phone]
+		[Display(Name = "Phone number:")]
+		public string PhoneNumber { get; set; } = string.Empty;
 	}
 }
