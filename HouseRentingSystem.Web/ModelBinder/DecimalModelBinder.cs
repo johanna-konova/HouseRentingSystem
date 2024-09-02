034ec22dec6c5dfc240388480238767289f2418a
johanna-konova <j.konova@abv.bg>
2024-09-02 15:26:03 +0300

    DecimalModelBinderProvider created

---
+++ HouseRentingSystem.Web/ModelBinder/DecimalModelBinder.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+
+namespace HouseRentingSystem.Web.ModelBinder
+{
+	public class DecimalModelBinder : IModelBinder
+	{
+		public Task BindModelAsync(ModelBindingContext bindingContext)
+		{
+			var valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None
+				&& !string.IsNullOrEmpty(valueResult.FirstValue))
+            {
+				decimal result = 0.00M;
+				bool success = false;
+
+				try
+				{
+					string valueAsString = valueResult.FirstValue;
+					valueAsString = valueAsString.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+					valueAsString = valueAsString.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+					result = Convert.ToDecimal(valueAsString);
+					success = true;
+				}
+				catch (FormatException fe)
+				{
+					bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+				}
+
+                if (success)
+                {
+					bindingContext.Result = ModelBindingResult.Success(result);
+				}
+            }
+
+			return Task.CompletedTask;
+        }
+	}
+}
