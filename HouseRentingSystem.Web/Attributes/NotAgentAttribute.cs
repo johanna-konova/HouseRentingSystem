628ac0ce749f4419659d2f4e4c4c4837a3949dae
johanna-konova <j.konova@abv.bg>
2024-09-05 15:52:29 +0300

    NonAgentAttribute code refactored

--- HouseRentingSystem.Web/Attributes/NotAgentAttribute.cs
+++ HouseRentingSystem.Web/Attributes/NotAgentAttribute.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Security.Claims;
 
+using static HouseRentingSystem.Web.Attributes.Common.CommonFunctionalities;
 using static HouseRentingSystem.Core.Constants.MessageTypes;
 using static HouseRentingSystem.Core.Constants.MessageConstants;
 
@@ -22,13 +23,9 @@ namespace HouseRentingSystem.Web.Attributes
 				return;
 			}
 
-            if (await agentService!.IsAgentAsync(context.HttpContext.User.Id()))
+			if (await agentService!.IsAgentAsync(context.HttpContext.User.Id()))
 			{
-				var controller = (Controller)context.Controller;
-				controller.TempData[ErrorMessage] = AlreadyAgent;
-
-				context.Result = new RedirectToActionResult(nameof(HouseController.Mine), "House", null);
-				return;
+				HandleError(context, ErrorMessage, AlreadyAgent, nameof(HouseController.Mine), "House");
 			}
 
             await next();
