c273f3fe44b7ee9616a5a0c0c4ebb97aece8602b
johanna-konova <j.konova@abv.bg>
2024-09-06 14:25:13 +0300

    RentedAttribute created

---
+++ HouseRentingSystem.Web/Attributes/RentedAttribute.cs
@@ -0,0 +1,40 @@
+﻿using HouseRentingSystem.Controllers;
+using HouseRentingSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Core.Constants.MessageTypes;
+using static HouseRentingSystem.Web.Attributes.Common.CommonFunctionalities;
+
+namespace HouseRentingSystem.Web.Attributes
+{
+    public class RentedAttribute : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Guid houseId = ParseId(context);
+
+            if (houseId != Guid.Empty)
+            {
+                IHouseService? houseService =
+                    context.HttpContext.RequestServices.GetService<IHouseService>();
+
+                if (houseService == null)
+                {
+                    context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                }
+
+                Guid userId = context.HttpContext.User.Id();
+
+                if (await houseService!.IsRentedByUserWithGivenId(houseId, userId))
+                {
+                    await next();
+                    return;
+                }
+            }
+
+            HandleError(context, ErrorMessage, OnlyPropertyRenterIsAllowedToLeaveIt, nameof(HouseController.All), "House");
+        }
+    }
+}
