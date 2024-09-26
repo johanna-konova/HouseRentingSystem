57667f47bb99ef69891ad3bac4515052d503cf8d
johanna-konova <j.konova@abv.bg>
2024-09-26 13:02:04 +0300

    StatisticApiController added

---
+++ HouseRentingSystem.Web/Controllers/Api/StatisticApiController.cs
@@ -0,0 +1,28 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.Statistics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/statistic")]
+    public class StatisticApiController : ControllerBase
+    {
+        private readonly IStatisticsService statisticsService;
+
+        public StatisticApiController(IStatisticsService _statisticsService)
+        {
+            statisticsService = _statisticsService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(HousesStatisticModel))]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetHousesStatistic()
+        {
+            var housesStatistic = await statisticsService.GetHousesStatisticAsync();
+
+            return Ok(housesStatistic);
+        }
+    }
+}
